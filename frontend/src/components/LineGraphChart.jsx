874712e126b0c8ba3af035f3dcb5539db02260a1
sayano <sayano.tomomatsu@outlook.jp>
2026-05-04 13:51:51 +0900

    時間軸のグラフのデザイン変更

--- frontend/src/components/LineGraphChart.jsx
+++ frontend/src/components/LineGraphChart.jsx
@@ -1,20 +1,60 @@
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
-function LineGraphChart(){
-    const data = [
-        { time: '1', love: 400 },
-        { time: '2', love: 300 },
-        { time: '3', love: 600 },
-        { time: '4', love: 800 },
-    ];
-    return (
-    <div className="line-chart-div" style={{ width: '100%', height: 300 }}>
-      <p>折れ線グラフの表示</p>
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+function LineGraphChart() {
+  const rawData = [
+    { message: "ありがとう", love: 50 },
+    { message: "OK", love: 30 },
+    { message: "さよなら", love: 10 },
+    { message: "ごめんね", love: 80 },
+  ];
+  const data = rawData.map((item, index) => ({
+    index: index + 1,
+    ...item,
+  }));
+
+  return (
+    <div className="line-chart-div" style={{ width: "100%", height: 300 }}>
+      <h2 style={{ color: "#f28482" }}>時間軸でみる親密度♥</h2>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data}>
+        <LineChart
+          data={data}
+          margin={{ top: 20, right: 30, left: 30, bottom: 70 }}
+        >
+          <Tooltip
+            formatter={(value) => `${value}%`}
+            labelFormatter={(label, payload) => {
+              if (payload && payload.length > 0) {
+                return ` ${payload[0].payload.message}`;
+              }
+              return label;
+            }}
+          />
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Line type="monotone" dataKey="love" stroke="#8884d8" />
+          <XAxis
+            dataKey="index"
+            label={{
+              value: "トーク回数",
+              position: "insideBottom",
+              offset: -5,
+            }}
+          />
+          <YAxis
+            domain={[0, 100]} //
+            label={{ value: "親密度(%)", angle: -90, position: "insideLeft" }}
+          />
+          <Line
+            type="monotone"
+            dataKey="love"
+            stroke="#f28482"
+            strokeWidth={3}
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
